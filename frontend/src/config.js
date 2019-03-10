44dcba08f05f0322a50da968aa04d95a8025a4fe
Vick Vasquez <vasjvic44@gmail.com>
2019-03-09 22:18:13 -0600

    Initial setup frontend

---
+++ frontend/src/config.js
@@ -0,0 +1,10 @@
+const apiHost = window.apiHost || process.env.API_HOST || 'http://localhost:3000';
+
+const config = {
+  apiHost,
+  title: 'A texto',
+  port: process.env.PORT || 4000,
+  env: window.env || process.env.NODE_ENV,
+}
+
+export default config;
